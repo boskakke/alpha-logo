fa58a97e5824541e5ce66ec033fd817c5bc5d5e5
Bo Skakke <bo@skakke.dk>
2020-09-08 15:28:46 +0200

    logo

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -4,8 +4,6 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 gsap.registerPlugin(ScrollTrigger);
 
-
-
 const logo = document.querySelectorAll('.logo')
 const tl_logo = gsap.timeline()
 
@@ -13,24 +11,23 @@ const rotateFrom = (index) => {
 	if(index == 0) {
 		return '0deg';
 	} else {
-		return 179 / 8 * (index - 1) + 'deg';
+		return 179.9 / 8 * (index - 1) + 'deg';
 	}
 }
 const rotateTo = (index) => {
-		return 179 / 8 * index + 'deg';
+		return 179.9 / 8 * index + 'deg';
 }
 
-logo.forEach((leaf, index) => {	
-	tl_logo.fromTo(leaf, {opacity: 0, rotate: rotateFrom(index)},{opacity: 1, rotate: rotateTo(index), ease: 'power1.out', duration: 2})
+logo.forEach((leaf, i) => {	
+	tl_logo.fromTo(leaf, {opacity: 0, rotate: rotateFrom(i)},{opacity: 1, rotate: rotateTo(i), ease: 'power1.out', duration: 2}, '<.5')
 })
 
-//transform: rotate(calc(180deg / 8 * #{$i}));	
 
 ScrollTrigger.create({
 	animation: tl_logo,
   trigger: ".trigger",
   start: "top center",
-  end: "top 100px",
+  end: "bottom center",
   // markers: {startColor: "black", endColor: "black"},
   scrub: true,
   pin: '.container',
