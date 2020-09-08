9c9546460144cba8b79d7f1601ba0714522a52e3
Bo Skakke <bo@skakke.dk>
2020-09-08 14:49:37 +0200

    logo

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -9,10 +9,24 @@ gsap.registerPlugin(ScrollTrigger);
 const logo = document.querySelectorAll('.logo')
 const tl_logo = gsap.timeline()
 
-logo.forEach((leaf) => {
-	tl_logo.fromTo(leaf, {opacity: 0},{opacity: 1})
+const rotateFrom = (index) => {
+	if(index == 0) {
+		return '0deg';
+	} else {
+		return 179 / 8 * (index - 1) + 'deg';
+	}
+}
+const rotateTo = (index) => {
+		return 179 / 8 * index + 'deg';
+}
+
+logo.forEach((leaf, index) => {
+	
+tl_logo.fromTo(leaf, {opacity: 0, rotate: rotateFrom(index)},{opacity: 1, rotate: rotateTo(index), ease: 'none'})
+	
 })
 
+//transform: rotate(calc(180deg / 8 * #{$i}));	
 
 ScrollTrigger.create({
 	animation: tl_logo,
